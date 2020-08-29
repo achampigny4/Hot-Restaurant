b1636a856952549fb778fc879b454623f183dbcf
Keith Tharp <keithstharp@gmail.com>
2020-08-29 11:03:36 -0400

    created data objects

---
+++ data/tabelData.js
@@ -0,0 +1,13 @@
+// Object for reservations
+
+const reservedTables = [
+  {
+    id: "id",
+    name: "name",
+    phoneNumber: "123-456-7890",
+    email: "email@email.com"
+  }
+
+]
+
+module.exports = reservedTables;
---
+++ data/waitlistData.js
@@ -0,0 +1,13 @@
+// waitinglist object
+
+const waitlistTables = [
+  {
+    id: "id",
+    name: "name",
+    phoneNumber: "123-456-7890",
+    email: "email@email.com"
+  }
+
+]
+
+module.exports = waitlistTables;
